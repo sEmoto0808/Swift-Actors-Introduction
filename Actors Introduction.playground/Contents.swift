ac8d425b59f94c1c2f3d681a7bc2f2da9bf91cd4
Sho Emoto <dd2d00seg1@gmail.com>
2022-01-03 21:29:52 +0900

    クラスを定義

--- Actors Introduction.playground/Contents.swift
+++ Actors Introduction.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class User {
+
+    let id = UUID().uuidString
+    let name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class UserStorage {
+
+    private var store = [String: User]()
+
+    func get(_ id: String) -> User? {
+        store[id]
+    }
+
+    func save(_ user: User) {
+        store[user.id] = user
+    }
+}
